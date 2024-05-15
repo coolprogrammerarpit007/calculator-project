20a52da7a33432333615580fa1472f0c7674575a
Arpit <arpit.mishra.out@gmail.com>
2024-05-15 20:00:31 +0530

    Added all the features and calculator's all basic operations are working!

--- js/script.js
+++ js/script.js
@@ -7,7 +7,7 @@ const displayText = document.querySelector(`.display`);
 const calculatorBtns = document.querySelectorAll(`.btn`);
 const numberBtns = document.querySelectorAll(`.number`);
 const operatorBtns = document.querySelectorAll(`.operator`);
-const methodBtns = document.querySelectorAll(`.method`);
+// const methodBtns = document.querySelectorAll(`.method`);
 const equalsOp = document.querySelector(`.equals`);
 
 // ****************************************
@@ -18,7 +18,7 @@ let operator;
 let num2;
 let str = ``;
 let flag = false;
-let total = 0;
+let total;
 
 const addition = function (num1, num2) {
   total = num1 + num2;
@@ -60,26 +60,21 @@ const display = function (value) {
 };
 
 const displayOperator = function (value) {
-  console.log(`display operator`);
   displayText.textContent = value;
   flag = true;
   str = ``;
-  console.log(flag);
 };
 
 numberBtns.forEach((btn) => {
   btn.addEventListener(`click`, function (e) {
     if (flag) {
-      console.log(str);
       str += btn.textContent;
       display(str);
       num2 = str;
-      console.log(`Number 2: ${num2}`);
     } else {
       str += btn.textContent;
       display(str);
       num1 = str;
-      console.log(`Number 1:${num1} `);
     }
   });
 });
@@ -95,6 +90,10 @@ operatorBtns.forEach((btn) => {
 // Adding funcnaity to the equals operator
 
 equalsOp.addEventListener(`click`, function (e) {
+  if (total) {
+    num1 = total;
+    console.log(num1);
+  }
   num1 = Number(num1);
   num2 = Number(num2);
   operate(num1, num2, operator);
